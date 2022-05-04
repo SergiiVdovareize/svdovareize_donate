6d15211bbe42e46131f8aeb83b6f24c8870f5d7d
Sergey Vdovareize <sergey.vdovareize@inspera.no>
2022-05-04 17:43:38 +0300

    new phase

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /**
  * TODO list:
- * 1. validate card numbers in html and js
+ * 1. validate card numbers
  * 2. validate private link
  * 3. validate mono link
  * 4. validate revolute link
@@ -9,31 +9,4 @@
  * 6. update donate description
  */
 
-(()=>{
-    let resetId
-
-    const copyNumber = (block) => {
-        clearTimeout(resetId);
-        block.classList.add('copied')
-        
-        const cardNumber = document.getElementById('card-number').innerText.replaceAll(/\s/g, "")
-        navigator.clipboard.writeText(cardNumber)
-
-        resetId = setTimeout(() => {
-            resetCopy(block)
-        }, 10000)
-    }
-
-    const resetCopy = (block) => {
-        block.classList.remove('copied')
-    }
-
-    const initCopy = () => {
-        const block = document.getElementById('card-number-block')
-        block.onclick = () => {
-            copyNumber(block)
-        }
-    }
-
-    initCopy()
-})()
+ (()=>{let e;const t=e=>{e.classList.remove("copied")};(()=>{const c=document.getElementById("card-number-block");c.onclick=(()=>{(c=>{clearTimeout(e),c.classList.add("copied");const o=document.getElementById("card-number").innerText.replaceAll(/\s/g,"");navigator.clipboard.writeText(o),e=setTimeout(()=>{t(c)},1e4)})(c)})})()})();
