b99579c4d9814f99adf1720a942485bacc45b7b8
Sergii Vdovareyze <servdo@gmail.com>
2022-05-06 07:51:18 +0300

    vibration

--- main.js
+++ main.js
@@ -9,4 +9,4 @@
  * 6. update donate description
  */
 
- (()=>{let e;const t=e=>{e.classList.remove("copied")};(()=>{const c=document.getElementById("card-number-block");c.onclick=(()=>{(c=>{clearTimeout(e),c.classList.add("copied");const o=document.getElementById("card-number").innerText.replaceAll(/\s/g,"");navigator.clipboard.writeText(o),e=setTimeout(()=>{t(c)},1e4)})(c)})})()})();
+ (()=>{let e;const t=e=>{e.classList.remove("copied")};(()=>{const c=document.getElementById("card-number-block");c.onclick=(()=>{(c=>{clearTimeout(e),c.classList.add("copied");const o=document.getElementById("card-number").innerText.replaceAll(/\s/g,"");navigator.clipboard.writeText(o),navigator.vibrate(50),e=setTimeout(()=>{t(c)},1e4)})(c)})})()})();
